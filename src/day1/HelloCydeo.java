0786304323cb36e2ef1ec23f1aae4710a20bd1ed
ouma122 <oumaima.mett@gmail.com>
2025-03-17 10:47:22 -0400

    Update HelloCydeo.java

--- src/day1/HelloCydeo.java
+++ src/day1/HelloCydeo.java
@@ -4,6 +4,7 @@ public class HelloCydeo {
 
     public static void main(String[] args) {
         System.out.println("Hello Cydeo");
+        System.out.pritln("Hello Cydeo2");
 
         // Some changes are added
         
