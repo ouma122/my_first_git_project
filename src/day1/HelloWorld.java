8928315f77ca184fcb0a7fd86cd8989938fa0964
ouma122 <oumaima.mett@gmail.com>
2025-03-17 10:29:31 -0400

    Update HelloWorld.java

--- src/day1/HelloWorld.java
+++ src/day1/HelloWorld.java
@@ -4,6 +4,9 @@ public class HelloWorld {
     public static void main(String[] args) {
 
         System.out.println("Hello World");
+        System.out.pritln("Hello Programmers");
+        System.out.println("Hello Cydeo");
+        
     }
 
 }
